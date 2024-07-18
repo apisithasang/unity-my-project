e399495c4e0b1d27ea6f724d8ec4c2a51428eae8
apisithasang <apisithasang@gmail.com>
2024-07-18 10:15:36 +0700

    main

--- Player.cs
+++ Player.cs
@@ -8,19 +8,28 @@ public class Player : MonoBehaviour
     private CharacterController character;
     public float speed = 30;
     private Vector3 MoveDirection;
-
+    
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         character = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(character.isGrounded){
+
+            anim.SetBool("IsWalk",false);
             MoveDirection = new Vector3(Input.GetAxis("Horizontal"),0.0f,Input.GetAxis("Vertical"));
             MoveDirection *= speed;
+            
+            if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
+                anim.SetBool("IsWalk",true);
+            }
+            
             character.Move(MoveDirection*Time.deltaTime);
         }
     }
