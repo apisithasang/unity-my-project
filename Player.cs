fbe38a11ffa1258ee605a7a31d5f8f81b31f696f
apisithasang <apisithasang@gmail.com>
2024-07-18 22:33:33 +0700

    AnimationControllerRotate and Comment

--- Player.cs
+++ Player.cs
@@ -21,40 +21,46 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Player Character Check Floor
        if(chr.isGrounded){
+        // Player animation Idle When IsWalk is false
         anima.SetBool("IsWalk",false);
          MD = new Vector3(Input.GetAxis("Horizontal"),0.0f,Input.GetAxis("Vertical"));
         MD *= speed;
         
         if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
+                // Player animation Walk When IsWalk is true
             anima.SetBool("IsWalk",true);
         }
         // Rotation Player Function
         Rotate();
+        // Player Character Get Move Function Add Parameter MD Calurator Time.deltTime For Smooth Movement.
         chr.Move(MD * Time.deltaTime);
        }
     }
 
-
     void Rotate(){
-
-        // Rotation Player
-        if(Input.GetAxis("Horizontal") <0){
-
+        
+        // Left Direction
+        if(Input.GetAxis("Horizontal") < 0){
             this.transform.rotation = Quaternion.Euler(0.0f,-90f,0.0f);
         }
-
-        if(Input.GetAxis("Horizontal") >0){
+        // Right Direction
+        if(Input.GetAxis("Horizontal") >0 ){
             this.transform.rotation = Quaternion.Euler(0.0f,90f,0.0f);
         }
-
-        if(Input.GetAxis("Vertical") >0){
+        // Back Direction
+        if(Input.GetAxis("Vertical") < 0){
+            this.transform.rotation = Quaternion.Euler(0.0f,180f,0.0f);
+        }
+        // Front Direction
+        if(Input.GetAxis("Vertical") > 0){
             this.transform.rotation = Quaternion.Euler(0.0f,0.0f,0.0f);
         }
 
-        if(Input.GetAxis("Vertical") <0){
-            this.transform.rotation = Quaternion.Euler(0.0f,-180f,0.0f);
-        }
 
     }
+
+
+   
 }
